df6cc527b00b485d8617545721ea808be563a08c
Henrique Guarnieri <henrique.guarnieri@gmail.com>
2016-04-17 21:41:27 -0300

    Fixings

--- ImportAddresses.py
+++ ImportAddresses.py
@@ -15,42 +15,73 @@ db = MySQLdb.connect(host="localhost",
 
 cur = db.cursor()
 
-for numero in range(1, 71):
+for numero in range(1, 70):
 
     numeroLinha = str(numero).zfill(2)
     url = 'http://www.athenaspaulista.com.br/LINHAS/Linha' + numeroLinha + '.htm'
+    timeToGo = ''
+    timeToGoBack = ''
+
+    # Correcoes
+    whereToSplit = [2, 4, 6]
+    if numero == 44 or numero == 52:
+        whereToSplit = [2, 3, 4]
+
+    whereToSplitTitle = "-"
+    if numero == 58 or numero == 41:
+        whereToSplitTitle = "\xe2\x80\x93"
+
+    divider = ";"
+    if numero == 60:
+        divider = ","
 
     # Delete old values
     cur.execute("DELETE FROM enderecos WHERE linha=" + numeroLinha)
 
     try:
         response = urllib2.urlopen(url)
-        html = response.read().decode('windows-1252').encode('utf-8')
+        html = response.read().decode('windows-1252').encode('utf-8').replace("&nbsp;", "").replace('\r\n', '').replace("	", "").replace("  ", " ")
 
-        unclearedTitle = html.split('<tr style=\'mso-yfti-irow:0;mso-yfti-firstrow:yes;mso-yfti-lastrow:yes\'>')[2].split("</tr>")[0]
-        title = remove_tags(unclearedTitle).split("-")[1].strip()
+        unclearedTitle = html.split('<tr style=\'mso-yfti-irow:0;mso-yfti-firstrow:yes;mso-yfti-lastrow:yes\'>')[whereToSplit[0]].split("</tr>")[0]
+        title = remove_tags(unclearedTitle).split(whereToSplitTitle, 1)[1].strip()
 
         print title
 
-        unclearedAddressesToGo = html.split('<tr style=\'mso-yfti-irow:0;mso-yfti-firstrow:yes;mso-yfti-lastrow:yes\'>')[4].split("</tr>")[0]
-        addressesToGo = remove_tags(unclearedAddressesToGo).strip().replace("\n", "")
-
+        unclearedAddressesToGo = html.split('<tr style=\'mso-yfti-irow:0;mso-yfti-firstrow:yes;mso-yfti-lastrow:yes\'>')[whereToSplit[1]].split("</tr>")[0]
+        if numero == 44:
+            unclearedAddressesToGo = unclearedAddressesToGo.split("<p class=MsoPlainText style='text-align:justify'>")[2].split("</p>")[0]
+        if numero == 60:
+            unclearedAddressesToGo = unclearedAddressesToGo.split(")", 1)[1]
+        addressesToGo = remove_tags(unclearedAddressesToGo).strip()
         print addressesToGo
 
         ordem = 0
-        for address in addressesToGo.split(";"):
-            cur.execute("INSERT INTO enderecos VALUES(%s, %s, %s, %s)", (numeroLinha, ordem, 1, address))
-            ordem += 1
+        for address in addressesToGo.split(divider):
+            if address[-1] == '.':
+                address = address[-1:]
+            if len(address) > 3:
+                cur.execute("INSERT INTO enderecos VALUES(%s, %s, %s, %s)", (numeroLinha, ordem, 1, address.strip()))
+                ordem += 1
 
-        unclearedAddressesToGoBack = html.split('<tr style=\'mso-yfti-irow:0;mso-yfti-firstrow:yes;mso-yfti-lastrow:yes\'>')[6].split("</tr>")[0]
-        addressesToGoBack = remove_tags(unclearedAddressesToGoBack).strip().replace("\n", "")
+        try:
+            unclearedAddressesToGoBack = html.split('<tr style=\'mso-yfti-irow:0;mso-yfti-firstrow:yes;mso-yfti-lastrow:yes\'>')[whereToSplit[2]].split("</tr>")[0]
+            if numero == 44:
+                unclearedAddressesToGoBack = unclearedAddressesToGoBack.split("<p class=MsoPlainText style='text-align:justify'>")[2].split("</p>")[0]
+            if numero == 60:
+                unclearedAddressesToGoBack = unclearedAddressesToGoBack.split(")", 1)[1]
+            addressesToGoBack = remove_tags(unclearedAddressesToGoBack).strip()
 
-        print addressesToGoBack
+            print addressesToGoBack
 
-        ordem = 0
-        for address in addressesToGo.split(";"):
-            cur.execute("INSERT INTO enderecos VALUES(%s, %s, %s, %s)", (numeroLinha, ordem, 2, address))
-            ordem += 1
+            ordem = 0
+            for address in addressesToGo.split(divider):
+                if address[-1] == '.':
+                    address = address[-1:]
+                if len(address) > 3:
+                    cur.execute("INSERT INTO enderecos VALUES(%s, %s, %s, %s)", (numeroLinha, ordem, 2, address.strip()))
+                    ordem += 1
+        except Exception, e:
+            print ''
 
     except urllib2.HTTPError, err:
         if err.code == 404:
@@ -61,8 +92,6 @@ for numero in range(1, 71):
     db.commit()
 
 # Cleanings
-cur.execute("UPDATE enderecos SET endereco = REPLACE(endereco, '\r', '')")
-cur.execute("UPDATE enderecos SET endereco = REPLACE(endereco, '  ', ' ')")
 cur.execute("UPDATE enderecos SET endereco = TRIM(endereco)")
 
 cur.close()
--- ImportLinhas.py
+++ ImportLinhas.py
@@ -19,35 +19,54 @@ for numero in range(1, 70):
 
     numeroLinha = str(numero).zfill(2)
     url = 'http://www.athenaspaulista.com.br/LINHAS/Linha' + numeroLinha + '.htm'
+    timeToGo = ''
+    timeToGoBack = ''
+
+    # Correcoes
+    whereToSplit = [2, 3, 5]
+    if numero == 44 or numero == 52:
+        whereToSplit = [2, 3, 4]
+
+    whereToSplitTitle = "-"
+    if numero == 58 or numero == 41:
+        whereToSplitTitle = "\xe2\x80\x93"
 
     # Delete old values
     cur.execute("DELETE FROM linhas WHERE numero=" + numeroLinha)
 
     try:
         response = urllib2.urlopen(url)
-        html = response.read().decode('windows-1252').encode('utf-8')
+        html = response.read().decode('windows-1252').encode('utf-8').replace("&nbsp;", "").replace('\r\n', '').replace("	", "")
 
-        unclearedTitle = html.split('<tr style=\'mso-yfti-irow:0;mso-yfti-firstrow:yes;mso-yfti-lastrow:yes\'>')[2].split("</tr>")[0]
-        title = remove_tags(unclearedTitle).split(" -")[1].strip()
+        unclearedTitle = html.split('<tr style=\'mso-yfti-irow:0;mso-yfti-firstrow:yes;mso-yfti-lastrow:yes\'>')[whereToSplit[0]].split("</p>")[0]
+        title = remove_tags(unclearedTitle).split(whereToSplitTitle, 1)[1].strip()
 
-        print title
-
-        unclearedTimeToGo = html.split('<tr style=\'mso-yfti-irow:0;mso-yfti-firstrow:yes;mso-yfti-lastrow:yes\'>')[3].split("</tr>")[0]
+        unclearedTimeToGo = html.split('<tr style=\'mso-yfti-irow:0;mso-yfti-firstrow:yes;mso-yfti-lastrow:yes\'>')[whereToSplit[1]].split("</p>")[0]
         timeToGo = remove_tags(unclearedTimeToGo).strip()
 
+        try:
+            unclearedTimeToGoBack = html.split('<tr style=\'mso-yfti-irow:0;mso-yfti-firstrow:yes;mso-yfti-lastrow:yes\'>')[whereToSplit[2]].split("</p>")[0]
+            timeToGoBack = remove_tags(unclearedTimeToGoBack).strip()
+        except:
+            timeToGoBack = '-'
+
+        if len(timeToGo) < 5:
+            timeToGo = '-'
+
+        if len(timeToGoBack) < 7:
+            timeToGoBack = '-'
+
+        print title
         print timeToGo
-
-        unclearedTimeToGoBack = html.split('<tr style=\'mso-yfti-irow:0;mso-yfti-firstrow:yes;mso-yfti-lastrow:yes\'>')[5].split("</tr>")[0]
-        timeToGoBack = remove_tags(unclearedTimeToGoBack).strip()
-
         print timeToGoBack
+        print "\n"
 
         cur.execute("INSERT INTO linhas VALUES(%s, %s, %s, %s, %s)", (numeroLinha, title, url, timeToGo, timeToGoBack))
     except urllib2.HTTPError, err:
         if err.code == 404:
-            print "Linha " + numeroLinha + " nao encontrada."
-    except:
-        print "Erro na linha: " + numeroLinha
+            print "Linha " + numeroLinha + " nao encontrada"
+    except Exception, e:
+        print "Erro na linha: " + numeroLinha + " " + str(e)
 
     db.commit()
 
