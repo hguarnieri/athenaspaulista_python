3b9f6eef17ec8b6c22fc769ec2a96b59fa6731f1
Victor <v_juliani@hotmail.com>
2016-04-21 15:35:51 -0300

    Fixed some cases. Export data to files.

--- ImportTime.py
+++ ImportTime.py
@@ -5,9 +5,9 @@ import re
 import urllib2
 
 # Functions
-def remove_tags(data):
+def remove_tags(replaceS, data):
     p = re.compile(r'<[^<]*?>')
-    return p.sub('<tag>', data)
+    return p.sub(replaceS, data)
 
 def isTime(value):
     try:
@@ -22,6 +22,10 @@ def parseTimes(data):
 
     i = 0
     while i < len(clearList):
+
+    	if i == 0 and "n\xc3\x83o opera" in clearList[i].lower():
+    		return times
+
         p = clearList[i]
         if i == (len(clearList) - 1):
             times.append([p, "-"])
@@ -37,7 +41,7 @@ def parseTimes(data):
 
     return times
 
-for numero in range(53, 54):
+for numero in range(1, 70):
 
     numeroLinha = str(numero).zfill(2)
     url = 'http://www.athenaspaulista.com.br/LINHAS/Linha' + numeroLinha + '.htm'
@@ -51,35 +55,65 @@ for numero in range(53, 54):
     if numero == 58 or numero == 41:
         whereToSplitTitle = "\xe2\x80\x93"
 
+    util = "DIAS ÚTEIS"
+    if numero == 34:
+    	util = "DIA ÚTEIS"
+
     try:
         response = urllib2.urlopen(url)
         html = response.read().decode('windows-1252').encode('utf-8').replace("&nbsp;", "").replace('\r\n', '').replace("	", "").replace("  ", " ")
 
         unclearedTitle = html.split('<tr style=\'mso-yfti-irow:0;mso-yfti-firstrow:yes;mso-yfti-lastrow:yes\'>')[whereToSplit[0]].split("</tr>")[0]
-        title = remove_tags(unclearedTitle).split(whereToSplitTitle, 1)[1].strip()
+        title = remove_tags('', unclearedTitle).split(whereToSplitTitle, 1)[1].strip()
 
-        print title + "\n"
+        print str(numero) + ": " + title + "\n"
 
-        clearHtml = remove_tags(html).strip()
-        sundays = clearHtml.split("DOMINGOS E FERIADOS") # [1]
+        clearHtml = remove_tags('<tag>', html).strip()
+
+        f = open("./times/" + str(numero) + ".txt", "w+")
+
+        if numero == 52:
+        	holiday = clearHtml.split("FERIADOS")
+        	f.write(str(parseTimes(holiday[1])))
+        	f.write('\n\n')
+        	sundays = holiday[0].split("DOMINGOS")
+        else:
+			sundays = clearHtml.split("DOMINGOS E FERIADOS") # [1]
+
+        if len(sundays) == 1:
+        	sundays = clearHtml.split("DOMINGO E FERIADO") # [1]
+
         saturdays = sundays[0].split("SÁBADOS") # [1]
-        allDays = saturdays[0].split("DIAS ÚTEIS") # [1]
 
-        print "Dias úteis"
-        print parseTimes(allDays[1])
-        print "\n\n"
+        if len(saturdays) == 1:
+        	saturdays = sundays[0].split("SÁBADO") # [1]
 
-        print "Sábados"
-        print parseTimes(saturdays[1])
-        print "\n\n"
+        allDays = saturdays[0].split(util) # [1]
 
-        print "Domingos e Feriados"
-        print parseTimes(sundays[1])
-        print "\n\n"
+        #print "Dias úteis"
+        #print parseTimes(allDays[1])
+        #print "\n\n"
 
-    except urllib2.HTTPError, err:
+        #print "Sábados"
+        #print parseTimes(saturdays[1])
+        #print "\n\n"
+
+        #print "Domingos e Feriados"
+        #print parseTimes(sundays[1])
+        #print "\n\n"
+        
+		# erros: 6, 18, 23, 27, 38, 39, 40, 45, 51, 54, 55
+
+        f.write(str(parseTimes(allDays[1])))
+        f.write('\n\n')
+        f.write(str(parseTimes(saturdays[1])))
+        f.write('\n\n')
+        f.write(str(parseTimes(sundays[1])))
+        f.close()
+
+    except urllib2.HTTPError as err:
         if err.code == 404:
             print "Linha " + numeroLinha + " nao encontrada."
-        print err
-    except:
+    except Exception as err:
         print "Erro na linha: " + numeroLinha
+        print err
